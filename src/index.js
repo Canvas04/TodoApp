89c05abf08c41a95f9b0bbfbe66c6d670d0ede10
Ruslan04071996 <ruslan040796@mail.ru>
2020-08-17 17:37:06 +0300

    начал верстку

--- src/index.js
+++ src/index.js
@@ -1,2 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+const Header = () => {
+  return (
+    <header className='header'>
+      <h1>todos</h1>
+      <input className='new-todo' placeholder='What needs to be done ?' autoFocus />
+    </header>
+  )
+}
+const TodoList = () => {
+  
+}
+const Main = () => {
+  return (
+    <section className='main'>
+
+    </section>
+  )
+}
+const App = () => {
+  return (
+    <section className='todoApp'>
+      <Header />
+      <Main />
+    </section>
+  )
+}
+
+ReactDOM.render(<App />, document.querySelector('#root'))
