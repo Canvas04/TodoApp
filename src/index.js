af4b69ebbda7ee5a5f2de8c8fa7b8a3c9d5d6249
Ruslan04071996 <ruslan040796@mail.ru>
2020-08-18 12:37:06 +0300

    Сделал базовую верстку

--- src/index.js
+++ src/index.js
@@ -9,13 +9,77 @@ const Header = () => {
     </header>
   )
 }
+const FilterList = () => {
+  return (
+    <ul className='filters'>
+      <li>
+        <button className='selected'>All</button>
+      </li>
+      <li>
+        <button>Active</button>
+      </li>
+      <li>
+        <button>Completed</button>
+      </li>
+    </ul>
+  )
+}
+const Footer = () => {
+  return (
+    <footer className='footer'>
+      <span className='todo-count'>1 items left</span>
+      <FilterList />
+      <button className='clear-completed'>Clear completed</button>
+    </footer>
+
+  )
+}
 const TodoList = () => {
-  
+  return (
+    <ul className='todo-list' >
+      <li className='completed'>
+        <div className='view'>
+          <input className='toggle' type='checkbox' />
+          <label>
+            <span className='description'>Completed task</span>{' '}
+            <span className='created'>created 17 seconds ago</span>
+          </label>
+          <button className='icon icon-edit'></button>
+          <button className='icon icon-destroy'></button>
+        </div>
+      </li>
+
+      <li className='editing'>
+        <div className='view'>
+          <input className='toggle' type='checkbox' />
+          <label>
+            <span className='description'>Completed task</span>{' '}
+            <span className='created'>created 5 minutes ago</span>
+          </label>
+          <button className='icon icon-edit'></button>
+          <button className='icon icon-destroy'></button>
+        </div>
+        <input type='text' className='edit' value='Editing task' />
+      </li>
+      <li>
+        <div className='view'>
+          <input className='toggle' type='checkbox' />
+          <label>
+            <span className='description'>Active task</span>{' '}
+            <span className='created'>created 5 minutes ago</span>
+          </label>
+          <button className='icon icon-edit'></button>
+          <button className='icon icon-destroy'></button>
+        </div>
+      </li>
+    </ul>
+  )
 }
 const Main = () => {
   return (
     <section className='main'>
-
+      <TodoList />
+      <Footer />
     </section>
   )
 }
