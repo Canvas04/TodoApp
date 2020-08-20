d669dffcbc4573b0017b7641cdcddbff94011031
Ruslan04071996 <ruslan040796@mail.ru>
2020-08-20 17:47:47 +0300

    Оптимизация кода

--- src/components/task/task.js
+++ src/components/task/task.js
@@ -8,37 +8,33 @@ class Task extends Component {
     done: false
   }
   onSpanClick = () => {
-    const {done} = this.state;
-    if(!done) {
-      this.setState({
-      done:true
+    this.setState((state) => {
+      return {
+        done: !state.done
+      }
     })
-  }else {
-    this.setState({
-      done:false
-    })
-  }
-    
-  }
 
-  render() {
-    const { done } = this.state;
-    let classNames = 'description';
-    if(done) {
-      classNames+= ' done';
-    }
-    return (
-      <div className='view'>
-        <input className='toggle' type='checkbox' />
-        <label>
-          <span className={classNames} onClick = {this.onSpanClick}>{this.props.state} </span>{' '}
-          <span className='created'>created {this.props.date} ago</span>
-        </label>
-        <button className='icon icon-edit'></button>
-        <button className='icon icon-destroy'></button>
-      </div>
-    )
+
+}
+
+render() {
+  const { done } = this.state;
+  let classNames = 'description';
+  if (done) {
+    classNames += ' done';
   }
+  return (
+    <div className='view'>
+      <input className='toggle' type='checkbox' />
+      <label>
+        <span className={classNames} onClick={this.onSpanClick}>{this.props.state} </span>{' '}
+        <span className='created'>created {this.props.date} ago</span>
+      </label>
+      <button className='icon icon-edit'></button>
+      <button className='icon icon-destroy'></button>
+    </div>
+  )
+}
 
 }
 
